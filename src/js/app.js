559440cf94f2723564125f11ecbb523bf31e69ff
asr <ajsoriar@gmail.com>
2019-04-14 03:19:26 +0200

    Upgrade library

--- src/js/app.js
+++ src/js/app.js
@@ -45,6 +45,15 @@ document.addEventListener("keydown", function (e) {
     if (e.keyCode === 13) { //checks whether the pressed key is "Enter"
         changeColor(e);
     }
+    if (e.keyCode === 55) {
+        ajsrConsole.autoHeight();
+    }
+    if (e.keyCode === 56) {
+        ajsrConsole.toggleOpacity();
+    }
+    if (e.keyCode === 57) {
+        ajsrConsole.moveNext();
+    }
 });
 
 function changeColor(e) {
@@ -55,7 +64,7 @@ function changeColor(e) {
 
 EasterEggs.show();
 EasterEggs.addSequence(null, [51, 50, 49], function () {
-    ajsrConsole.log("MATCH!!! 3,2,1");
+    log.log("MATCH!!! 3,2,1");
     changeColor();
 })
 
@@ -68,3 +77,9 @@ flyingCanvas.start({
     showWings: false,
     animation: true
 });
+
+ajsrConsole.toggleOpacity();
+ajsrConsole.useColors();
+log("yarn add purple-console", "cyan");
+log("[7]Auto height <br>[8]Opacity ON/OFF 8<br>[9]Move", "yellow");
+ajsrConsole.useColors(false);
